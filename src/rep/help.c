939cd52e59f81109e41e5e75c2cfca07520c0a29
hoefkensj <hoefkens.j@gmail.com>
2023-07-16 15:43:37 +0200

    rep, escape.c, escape.c~, help.c, and help.c~

--- src/rep/help.c
+++ src/rep/help.c
@@ -23,24 +23,24 @@ OPTIONS:\n\
     -v, --version     Show Version,About\n\
 \n\
 DOCS:\n\
-    <rep> and <str> may contain the following escape codes:\n\
-      \\                       Backslash\n\
-      \\a                      Alarm,Terminal Bell\n\
-      \\b                      Backspace\n\
-      \\f                      FormFeed\n\
-      \\n                      NewLine\n\
-      \\t                      Horizontal TAB\n\
-      \\v                      Vertical TAB\n\
-      \\r                      Carriage Return\n\
-      \\[0-7]{1,2,3}           Octal value (0-377)\n\
-      \\x[0-F]{2}              Hexadecimal value (BYTE)\n\
-      \\u                      Unicode (0000-FFFF)\n\
-      \\U                      Unicode (00000000-FFFFFFFF)\n\
+  Strings in arguments or options may contain the following escape codes:\n\
+  \\                       Backslash\n\
+  \\a                      Alarm,Terminal Bell\n\
+  \\b                      Backspace\n\
+  \\f                      FormFeed\n\
+  \\n                      NewLine\n\
+  \\t                      Horizontal TAB\n\
+  \\v                      Vertical TAB\n\
+  \\r                      Carriage Return\n\
+  \\[0-7]{1,2,3}           Octal value (0-377)\n\
+  \\x[0-F]{2}              Hexadecimal value (BYTE)\n\
+  \\u                      Unicode (0000-FFFF)\n\
+  \\U                      Unicode (00000000-FFFFFFFF)\n\
 \n\
-     This includes ANSI escape codes wich can be included using Octal(\\033) or Hexadecimal (\\x1b) escape codes.\n\
-     All Arguments support output from a subshell and the program can be run in one.\n\
-     This supports outputting to arguments of printf, or to the programs own arguments.\n\
-      \n\
+  This includes ANSI escape codes wich can be included using Octal(\\033) or Hexadecimal (\\x1b) escape codes.\n\
+  All Arguments support output from a subshell and the program can be run in one.\n\
+  This supports outputting to arguments of printf, or to the programs own arguments.\n\
+  \n\
 \n\
 "
 char *strUsage=USAGE;
@@ -50,8 +50,8 @@ char *strHelp=HELP;
 char *about(char *strAbout){
 	// char *keyval[];
 	char *mask="\x1b[%iG%s:\x1b[%iG%s%s";
-	char *kv[32];
-	char *aAb[6]={*kv,*kv,*kv,*kv,*kv,*kv};
+	char *kv="                                 ";
+	char *aAb[6]={kv,kv,kv,kv,kv,kv};
 
 	printf(mask,0  ,"VERSION" ,12 ,VERSION,"");
 	printf(mask,42 ,"DATE   " ,54 ,DATE   ,"\n");
