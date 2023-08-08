aa34a0e92d1c7ff0f591ccedc4a402b4fb6e8817
hoefkensj <hoefkens.j@gmail.com>
2023-08-08 18:41:21 +0200

    Makefile~, rep, argsParser.c, argsParser.c~, escape.c, escape.c~, argsParser.h, argsParser.h~, globals.h, globals.h~, main.h, main.h~, main.c, and main.c~

--- src/rep/headers/argsParser.h
+++ src/rep/headers/argsParser.h
@@ -2,8 +2,9 @@
 #include "globals.h"
 #include "escape.h"
 
-UI32 readPipe(ARGS *args,UI32 rcode);
+UI32 *readPipe(char *rep,UI32 *r);
 UI32 isOption(char *opt[3], char *arg);
-UI32 parseArgs(UI32 argc, char **argv,ARGS *args,UI32 rcode);
+UI32 parseArgs(UI32 argc, char **argv,ARGS *args,UI32 *nwbprsce[8]);
+
 
 
